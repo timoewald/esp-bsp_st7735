b4e6f94da970ca2f05b1045f5d1413b98862ad92
Timo Ewald <timo.ewald@web.de>
2025-03-04 22:34:52 +0100

    copied generic bsp

---
+++ bsp/esp_bsp_st7735/README.md
@@ -0,0 +1,235 @@
+# BSP: Generic
+
+[![Component Registry](https://components.espressif.com/components/espressif/esp_bsp_generic/badge.svg)](https://components.espressif.com/components/espressif/esp_bsp_generic)
+
+### Overview
+
+This is a generic BSP, which is configurable from `menuconfig`. The generic BSP can provide consistent API for simple boards, such as Espressif's DevKits. Predefined settings for selected DevKits are in [generic_button_led](examples/generic_button_led).
+
+**Supported features:**
+- I2C
+- SPIFFS
+- SD card
+- Buttons
+- LEDs
+
+# Build with predefined configuration
+
+Predefined configurations are saved in [generic_button_led](examples/generic_button_led) example.
+
+```
+    idf.py -p COM4 -D "SDKCONFIG_DEFAULTS=sdkconfig.esp32_s3_devkitc_1" flash monitor
+```
+
+# Example usage
+
+## I2C
+
+1. Set GPIOs for I2C in `menuconfig`
+    - `BSP_I2C_GPIO_SCL`
+    - `BSP_I2C_GPIO_SDA`
+
+2. Set I2C number in `menuconfig`
+    - `BSP_I2C_NUM`
+
+Example code:
+```
+    /* Initialization */
+    bsp_i2c_init();
+
+    /* Example I2C write with BSP_I2C_NUM */
+    i2c_master_write_to_device(BSP_I2C_NUM, 0x10, value, sizeof(value), 1000 / portTICK_PERIOD_MS);
+
+    ...
+
+    bsp_i2c_deinit();
+```
+**Note:** The BSP automatically initialize I2C, when need it for some component (LCD touch, audio, etc.)
+
+## SPIFFS
+
+Example code:
+```
+    /* Mount SPIFFS partition */
+    bsp_spiffs_mount();
+
+    /* Use file system read/write with BSP_SPIFFS_MOUNT_POINT */
+    FILE *in = fopen(BSP_SD_MOUNT_POINT"/text.txt", "rb");
+
+    ...
+
+    bsp_spiffs_unmount();
+```
+
+## SD card
+
+Example code:
+```
+    /* Mount SD card partition */
+    bsp_sdcard_mount();
+
+    /* Use file system read/write with BSP_SD_MOUNT_POINT */
+    FILE *in = fopen(BSP_SD_MOUNT_POINT"/text.txt", "rb");
+
+    ...
+
+    bsp_sdcard_unmount();
+```
+
+**Note:** This API is available only in MCUs, which have SD MMC peripheral.
+
+## Buttons
+
+1. Set count of buttons in `menuconfig`
+    - `BSP_BUTTONS_NUM` (max 5)
+
+2. Set button type and other values by type for each button in `menuconfig`
+    - `BSP_BUTTON_x_TYPE`
+    - `BSP_BUTTON_x_GPIO`           (for GPIO button)
+    - `BSP_BUTTON_x_LEVEL`          (for GPIO button)
+    - `BSP_BUTTON_x_ADC_CHANNEL`    (for ADC button)
+    - `BSP_BUTTON_x_ADC_VALUE`      (for ADC button)
+
+Example code:
+```
+    /* Button callback */
+    static void btn_handler(void *button_handle, void *usr_data)
+    {
+        int button_pressed = (int)usr_data;
+
+        ESP_LOGI(TAG, "Button pressed: %d", button_pressed);
+    }
+
+    /* Initialize all buttons and register callback for them */
+    button_handle_t btns[BSP_BUTTON_NUM];
+    ESP_ERROR_CHECK(bsp_iot_button_create(btns, NULL, BSP_BUTTON_NUM));
+    for (int i = 0; i < BSP_BUTTON_NUM; i++) {
+        ESP_ERROR_CHECK(iot_button_register_cb(btns[i], BUTTON_PRESS_DOWN, btn_handler, (void *) i));
+    }
+```
+For button handling is used component [iot_button](https://components.espressif.com/components/espressif/button). For more information, please look into guide for this component.
+
+## LEDS
+
+1. Set count of LEDs in `menuconfig`
+    - `BSP_LEDS_NUM` (max 5)
+
+2. Set type for all LEDs in `menuconfig`
+    - `BSP_LED_TYPE` (GPIO / Adressable RGB LED / Classic RGB)
+
+3. For GPIO LEDs set pin and level for each LED in `menuconfig`
+    - `BSP_LED_x_GPIO`
+    - `BSP_LED_x_LEVEL`
+
+3. For addressable RBG LEDs set pin and peripheral in `menuconfig`
+    - `BSP_LED_RGB_GPIO`
+    - `BSP_LED_RGB_BACKEND`
+
+3. For classic RBG LEDs set pins for all colors and level in `menuconfig`
+    - `BSP_LED_RGB_RED_GPIO`
+    - `BSP_LED_RGB_GREEN_GPIO`
+    - `BSP_LED_RGB_BLUE_GPIO`
+    - `BSP_LED_RGB_CLASSIC_LEVEL`
+
+Example code:
+```
+    /* Initialize all LEDs */
+    led_indicator_handle_t leds[BSP_LED_NUM];
+    ESP_ERROR_CHECK(bsp_led_indicator_create(leds, NULL, BSP_LED_NUM));
+
+    /* Set LED color for first LED (only for addressable RGB LEDs) */
+    led_indicator_set_rgb(leds[0], SET_IRGB(0, 0x00, 0x64, 0x64));
+
+    /* Start effect for each LED (predefined: BSP_LED_ON, BSP_LED_OFF, BSP_LED_BLINK_FAST, BSP_LED_BLINK_SLOW, BSP_LED_BREATHE_FAST, BSP_LED_BREATHE_SLOW) */
+    led_indicator_start(leds[0], BSP_LED_BREATHE_SLOW);
+```
+For LEDs handling is used component [led_indicator](https://components.espressif.com/components/espressif/led_indicator) with [led_strip](https://components.espressif.com/components/espressif/led_strip) component. For more information, please look into guides for these components.
+
+## LCD Display
+
+1. Enable display in `menuconfig`
+    - `BSP_DISPLAY_ENABLED`
+
+2. Select communication interface in `menuconfig`
+    - `BSP_DISPLAY_INTERFACE_` (only SPI is supported)
+
+3. Set communication pins in `menuconfig`
+    - `BSP_DISPLAY_SCLK_GPIO`
+    - `BSP_DISPLAY_MOSI_GPIO`
+    - `BSP_DISPLAY_MISO_GPIO`
+    - `BSP_DISPLAY_CS_GPIO`
+    - `BSP_DISPLAY_DC_GPIO`
+    - `BSP_DISPLAY_RST_GPIO`
+    - `BSP_DISPLAY_BACKLIGHT_GPIO`
+
+4. Select display driver in `menuconfig` (one of these)
+    - `BSP_DISPLAY_DRIVER_ST7789`
+    - `BSP_DISPLAY_DRIVER_ILI9341`
+    - `BSP_DISPLAY_DRIVER_GC9A01`
+
+5. Set right rotation of the screen in `menuconfig`
+    - `BSP_DISPLAY_ROTATION_SWAP_XY`
+    - `BSP_DISPLAY_ROTATION_MIRROR_X`
+    - `BSP_DISPLAY_ROTATION_MIRROR_Y`
+
+6. Set other display params in `menuconfig`
+    - `BSP_DISPLAY_CMD_BITS`
+    - `BSP_DISPLAY_PARAM_BITS`
+    - `BSP_DISPLAY_PIXEL_CLOCK`
+    - `BSP_DISPLAY_WIDTH`
+    - `BSP_DISPLAY_HEIGHT`
+    - `BSP_DISPLAY_BRIGHTNESS_LEDC_CH`
+    - `BSP_LCD_DRAW_BUF_HEIGHT`
+    - `BSP_LCD_DRAW_BUF_DOUBLE`
+
+## LCD Touch
+
+1. Enable display touch in `menuconfig`
+    - `BSP_TOUCH_ENABLED`
+
+2. Select communication interface in `menuconfig`
+    - `BSP_TOUCH_INTERFACE_` (only I2C is supported)
+
+3. Set communication pins in `menuconfig`
+    - `BSP_TOUCH_RST_GPIO`
+    - `BSP_TOUCH_INT_GPIO`
+
+4. Select display driver in `menuconfig` (one of these)
+    - `BSP_TOUCH_DRIVER_TT21100`
+    - `BSP_TOUCH_DRIVER_GT1151`
+    - `BSP_TOUCH_DRIVER_GT911`
+    - `BSP_TOUCH_DRIVER_CST816S`
+    - `BSP_TOUCH_DRIVER_FT5X06`
+
+5. Set right rotation of the screen in `menuconfig`
+    - `BSP_TOUCH_ROTATION_SWAP_XY`
+    - `BSP_TOUCH_ROTATION_MIRROR_X`
+    - `BSP_TOUCH_ROTATION_MIRROR_Y`
+
+Example code:
+```
+    /* Initialize display, touch and LVGL */
+    bsp_display_start();
+
+    /* Set display brightness to 100% */
+    bsp_display_backlight_on();
+
+    bsp_display_lock(0);
+    /* === Your LVGL code here === */
+    bsp_display_unlock();
+```
+<!-- Autogenerated start: Dependencies -->
+### Capabilities and dependencies
+|  Capability |     Available    |                                                   Component                                                  |  Version |
+|-------------|------------------|--------------------------------------------------------------------------------------------------------------|----------|
+|   DISPLAY   |:heavy_check_mark:|       [espressif/esp_lcd_gc9a01](https://components.espressif.com/components/espressif/esp_lcd_gc9a01)       |    ^1    |
+|  LVGL_PORT  |:heavy_check_mark:|        [espressif/esp_lvgl_port](https://components.espressif.com/components/espressif/esp_lvgl_port)        |    ^2    |
+|    TOUCH    |:heavy_check_mark:|[espressif/esp_lcd_touch_cst816s](https://components.espressif.com/components/espressif/esp_lcd_touch_cst816s)|    ^1    |
+|   BUTTONS   |:heavy_check_mark:|               [espressif/button](https://components.espressif.com/components/espressif/button)               |>=2.5,<4.0|
+|     LEDS    |:heavy_check_mark:|                                                                                                              |          |
+|    AUDIO    |        :x:       |                                                                                                              |          |
+|AUDIO_SPEAKER|        :x:       |                                                                                                              |          |
+|  AUDIO_MIC  |        :x:       |                                                                                                              |          |
+|    SDCARD   |        :x:       |                                                                                                              |          |
+|     IMU     |        :x:       |                                                                                                              |          |
+<!-- Autogenerated end: Dependencies -->
---
+++ bsp/esp_bsp_st7735/include/bsp/esp_bsp_generic.h
@@ -0,0 +1,404 @@
+/*
+ * SPDX-FileCopyrightText: 2023-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * @file
+ * @brief ESP BSP: Generic
+ */
+
+#pragma once
+
+#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "driver/i2c_master.h"
+#include "driver/sdmmc_host.h"
+#include "iot_button.h"
+#include "led_indicator.h"
+
+#if CONFIG_BSP_DISPLAY_ENABLED
+#include "bsp/display.h"
+#include "esp_lvgl_port.h"
+#endif //CONFIG_BSP_DISPLAY_ENABLED
+/**************************************************************************************************
+ *  BSP Capabilities
+ **************************************************************************************************/
+
+#if CONFIG_BSP_DISPLAY_ENABLED
+#define BSP_CAPS_DISPLAY    1
+#endif
+#if CONFIG_BSP_TOUCH_ENABLED
+#define BSP_CAPS_TOUCH      1
+#endif
+#if CONFIG_BSP_BUTTONS_NUM > 0
+#define BSP_CAPS_BUTTONS    1
+#endif
+#if CONFIG_BSP_LEDS_NUM > 0
+#define BSP_CAPS_LEDS       1
+#endif
+#define BSP_CAPS_AUDIO          0
+#define BSP_CAPS_AUDIO_SPEAKER  0
+#define BSP_CAPS_AUDIO_MIC      0
+#define BSP_CAPS_SDCARD         0
+#define BSP_CAPS_IMU            0
+
+/**************************************************************************************************
+ *  Pinout
+ **************************************************************************************************/
+/* I2C */
+#define BSP_I2C_SCL         (CONFIG_BSP_I2C_GPIO_SCL)
+#define BSP_I2C_SDA         (CONFIG_BSP_I2C_GPIO_SDA)
+
+/* SD card */
+#define BSP_SD_CMD          (CONFIG_BSP_SD_CMD)
+#define BSP_SD_CLK          (CONFIG_BSP_SD_CLK)
+#define BSP_SD_D0           (CONFIG_BSP_SD_D0)
+#define BSP_SD_D1           (CONFIG_BSP_SD_D1)
+#define BSP_SD_D2           (CONFIG_BSP_SD_D2)
+#define BSP_SD_D3           (CONFIG_BSP_SD_D3)
+
+/* Display */
+#define BSP_LCD_DATA0       (CONFIG_BSP_DISPLAY_MOSI_GPIO)
+#define BSP_LCD_PCLK        (CONFIG_BSP_DISPLAY_SCLK_GPIO)
+#define BSP_LCD_CS          (CONFIG_BSP_DISPLAY_CS_GPIO)
+#define BSP_LCD_DC          (CONFIG_BSP_DISPLAY_DC_GPIO)
+#define BSP_LCD_RST         (CONFIG_BSP_DISPLAY_RST_GPIO)
+#define BSP_LCD_BACKLIGHT   (CONFIG_BSP_DISPLAY_BACKLIGHT_GPIO)
+#define BSP_LCD_TOUCH_RST   (CONFIG_BSP_TOUCH_RST_GPIO)
+#define BSP_LCD_TOUCH_INT   (CONFIG_BSP_TOUCH_INT_GPIO)
+
+/* Buttons */
+#define BSP_BUTTON_1_IO     (CONFIG_BSP_BUTTON_1_GPIO)
+#define BSP_BUTTON_2_IO     (CONFIG_BSP_BUTTON_2_GPIO)
+#define BSP_BUTTON_3_IO     (CONFIG_BSP_BUTTON_3_GPIO)
+#define BSP_BUTTON_4_IO     (CONFIG_BSP_BUTTON_4_GPIO)
+#define BSP_BUTTON_5_IO     (CONFIG_BSP_BUTTON_5_GPIO)
+
+/* Leds */
+#define BSP_LED_1_IO        (CONFIG_BSP_LED_1_GPIO)
+#define BSP_LED_2_IO        (CONFIG_BSP_LED_2_GPIO)
+#define BSP_LED_3_IO        (CONFIG_BSP_LED_3_GPIO)
+#define BSP_LED_4_IO        (CONFIG_BSP_LED_4_GPIO)
+#define BSP_LED_5_IO        (CONFIG_BSP_LED_5_GPIO)
+
+/* Buttons */
+typedef enum {
+#if CONFIG_BSP_BUTTONS_NUM > 0
+    BSP_BUTTON_1,
+#endif
+#if CONFIG_BSP_BUTTONS_NUM > 1
+    BSP_BUTTON_2,
+#endif
+#if CONFIG_BSP_BUTTONS_NUM > 2
+    BSP_BUTTON_3,
+#endif
+#if CONFIG_BSP_BUTTONS_NUM > 3
+    BSP_BUTTON_4,
+#endif
+#if CONFIG_BSP_BUTTONS_NUM > 4
+    BSP_BUTTON_5,
+#endif
+    BSP_BUTTON_NUM
+} bsp_button_t;
+
+/* Leds */
+typedef enum {
+#if CONFIG_BSP_LEDS_NUM > 0
+    BSP_LED_1,
+#endif
+#if CONFIG_BSP_LEDS_NUM > 1
+    BSP_LED_2,
+#endif
+#if CONFIG_BSP_LEDS_NUM > 2
+    BSP_LED_3,
+#endif
+#if CONFIG_BSP_LEDS_NUM > 3
+    BSP_LED_4,
+#endif
+#if CONFIG_BSP_LEDS_NUM > 4
+    BSP_LED_5,
+#endif
+    BSP_LED_NUM
+} bsp_led_t;
+
+/* Default LED effects */
+enum {
+    BSP_LED_ON,
+    BSP_LED_OFF,
+    BSP_LED_BLINK_FAST,
+    BSP_LED_BLINK_SLOW,
+    BSP_LED_BREATHE_FAST,
+    BSP_LED_BREATHE_SLOW,
+    BSP_LED_MAX,
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#if CONFIG_BSP_DISPLAY_ENABLED
+/**
+ * @brief BSP display configuration structure
+ *
+ */
+typedef struct {
+    lvgl_port_cfg_t lvgl_port_cfg;
+} bsp_display_cfg_t;
+#endif //CONFIG_BSP_DISPLAY_ENABLED
+
+/**************************************************************************************************
+ *
+ * I2C interface
+ *
+ **************************************************************************************************/
+#define BSP_I2C_NUM     CONFIG_BSP_I2C_NUM
+
+/**
+ * @brief Init I2C driver
+ *
+ * @return
+ *      - ESP_OK                On success
+ *      - ESP_ERR_INVALID_ARG   I2C parameter error
+ *      - ESP_FAIL              I2C driver installation error
+ *
+ */
+esp_err_t bsp_i2c_init(void);
+
+/**
+ * @brief Deinit I2C driver and free its resources
+ *
+ * @return
+ *      - ESP_OK                On success
+ *      - ESP_ERR_INVALID_ARG   I2C parameter error
+ *
+ */
+esp_err_t bsp_i2c_deinit(void);
+
+/**
+ * @brief Get I2C driver handle
+ *
+ * @return
+ *      - I2C handle
+ */
+i2c_master_bus_handle_t bsp_i2c_get_handle(void);
+
+/**************************************************************************************************
+ *
+ * SPIFFS
+ *
+ * After mounting the SPIFFS, it can be accessed with stdio functions ie.:
+ * \code{.c}
+ * FILE* f = fopen(BSP_SPIFFS_MOUNT_POINT"/hello.txt", "w");
+ * fprintf(f, "Hello World!\n");
+ * fclose(f);
+ * \endcode
+ **************************************************************************************************/
+#define BSP_SPIFFS_MOUNT_POINT      CONFIG_BSP_SPIFFS_MOUNT_POINT
+
+/**
+ * @brief Mount SPIFFS to virtual file system
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_spiffs_register was already called
+ *      - ESP_ERR_NO_MEM if memory can not be allocated
+ *      - ESP_FAIL if partition can not be mounted
+ *      - other error codes
+ */
+esp_err_t bsp_spiffs_mount(void);
+
+/**
+ * @brief Unmount SPIFFS from virtual file system
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_NOT_FOUND if the partition table does not contain SPIFFS partition with given label
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_spiffs_unregister was already called
+ *      - ESP_ERR_NO_MEM if memory can not be allocated
+ *      - ESP_FAIL if partition can not be mounted
+ *      - other error codes
+ */
+esp_err_t bsp_spiffs_unmount(void);
+
+/**************************************************************************************************
+ *
+ * uSD card
+ *
+ * After mounting the uSD card, it can be accessed with stdio functions ie.:
+ * \code{.c}
+ * FILE* f = fopen(BSP_MOUNT_POINT"/hello.txt", "w");
+ * fprintf(f, "Hello %s!\n", bsp_sdcard->cid.name);
+ * fclose(f);
+ * \endcode
+ **************************************************************************************************/
+#define BSP_SD_MOUNT_POINT      CONFIG_BSP_SD_MOUNT_POINT
+
+#if SOC_SDMMC_HOST_SUPPORTED
+extern sdmmc_card_t *bsp_sdcard;
+#endif
+
+/**
+ * @brief Mount microSD card to virtual file system
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_fat_sdmmc_mount was already called
+ *      - ESP_ERR_NO_MEM if memory cannot be allocated
+ *      - ESP_FAIL if partition cannot be mounted
+ *      - other error codes from SDMMC or SPI drivers, SDMMC protocol, or FATFS drivers
+ */
+esp_err_t bsp_sdcard_mount(void);
+
+/**
+ * @brief Unmount microSD card from virtual file system
+ *
+ * @return
+ *      - ESP_OK on success
+ *      - ESP_ERR_NOT_FOUND if the partition table does not contain FATFS partition with given label
+ *      - ESP_ERR_INVALID_STATE if esp_vfs_fat_spiflash_mount was already called
+ *      - ESP_ERR_NO_MEM if memory can not be allocated
+ *      - ESP_FAIL if partition can not be mounted
+ *      - other error codes from wear levelling library, SPI flash driver, or FATFS drivers
+ */
+esp_err_t bsp_sdcard_unmount(void);
+
+
+#if CONFIG_BSP_DISPLAY_ENABLED
+/**************************************************************************************************
+ *
+ * LCD interface
+ *
+ * LVGL is used as graphics library. LVGL is NOT thread safe, therefore the user must take LVGL mutex
+ * by calling bsp_display_lock() before calling and LVGL API (lv_...) and then give the mutex with
+ * bsp_display_unlock().
+ *
+ * Display's backlight must be enabled explicitly by calling bsp_display_backlight_on()
+ **************************************************************************************************/
+#define BSP_LCD_PIXEL_CLOCK_HZ     (CONFIG_BSP_DISPLAY_PIXEL_CLOCK * 1000 * 1000)
+#define BSP_LCD_SPI_NUM            (SPI2_HOST)
+
+/**
+ * @brief Initialize display
+ *
+ * This function initializes SPI, display controller and starts LVGL handling task.
+ * LCD backlight must be enabled separately by calling bsp_display_brightness_set()
+ *
+ * @return Pointer to LVGL display or NULL when error occurred
+ */
+lv_display_t *bsp_display_start(void);
+
+/**
+ * @brief Initialize display
+ *
+ * This function initializes SPI, display controller and starts LVGL handling task.
+ * LCD backlight must be enabled separately by calling bsp_display_brightness_set()
+ *
+ * @param cfg display configuration
+ *
+ * @return Pointer to LVGL display or NULL when error occurred
+ */
+lv_display_t *bsp_display_start_with_config(const bsp_display_cfg_t *cfg);
+
+/**
+ * @brief Get pointer to input device (touch, buttons, ...)
+ *
+ * @note The LVGL input device is initialized in bsp_display_start() function.
+ *
+ * @return Pointer to LVGL input device or NULL when not initialized
+ */
+lv_indev_t *bsp_display_get_input_dev(void);
+
+/**
+ * @brief Take LVGL mutex
+ *
+ * @param timeout_ms Timeout in [ms]. 0 will block indefinitely.
+ * @return true  Mutex was taken
+ * @return false Mutex was NOT taken
+ */
+bool bsp_display_lock(uint32_t timeout_ms);
+
+/**
+ * @brief Give LVGL mutex
+ *
+ */
+void bsp_display_unlock(void);
+
+/**
+ * @brief Rotate screen
+ *
+ * Display must be already initialized by calling bsp_display_start()
+ *
+ * @param[in] disp Pointer to LVGL display
+ * @param[in] rotation Angle of the display rotation
+ */
+void bsp_display_rotate(lv_display_t *disp, lv_disp_rotation_t rotation);
+#endif //CONFIG_BSP_DISPLAY_ENABLED
+/**************************************************************************************************
+ *
+ * Button
+ *
+ **************************************************************************************************/
+
+/**
+ * @brief Initialize all buttons
+ *
+ * Returned button handlers must be used with espressif/button component API
+ *
+ * @note For LCD panel button which is defined as BSP_BUTTON_MAIN, bsp_display_start should
+ *       be called before call this function.
+ *
+ * @param[out] btn_array      Output button array
+ * @param[out] btn_cnt        Number of button handlers saved to btn_array, can be NULL
+ * @param[in]  btn_array_size Size of output button array. Must be at least BSP_BUTTON_NUM
+ * @return
+ *     - ESP_OK               All buttons initialized
+ *     - ESP_ERR_INVALID_ARG  btn_array is too small or NULL
+ *     - ESP_FAIL             Underlaying iot_button_create failed
+ */
+esp_err_t bsp_iot_button_create(button_handle_t btn_array[], int *btn_cnt, int btn_array_size);
+
+/**************************************************************************************************
+ *
+ * LEDs
+ *
+ **************************************************************************************************/
+
+/**
+ * @brief Initialize all LEDs
+ *
+ * @param[out] led_array      Output LED array
+ * @param[out] led_cnt        Number of LED handlers saved to led_array, can be NULL
+ * @param[in]  led_array_size Size of output LED array. Must be at least BSP_LED_NUM
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
+ */
+esp_err_t bsp_led_indicator_create(led_indicator_handle_t led_array[], int *led_cnt, int led_array_size);
+
+/**
+ * @brief Turn LED on/off
+ *
+ * @param handle led handle
+ * @param on Switch LED on/off
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
+ */
+esp_err_t bsp_led_set(led_indicator_handle_t handle, const bool on);
+
+/**
+ * @brief Set LED temperature
+ *
+ * @param handle led handle
+ * @param temperature Color temperature of LED
+ * @return
+ *     - ESP_OK Success
+ *     - ESP_ERR_INVALID_ARG Parameter error
+ */
+esp_err_t bsp_led_set_temperature(led_indicator_handle_t handle, const uint16_t temperature);
+
+#ifdef __cplusplus
+}
+#endif
